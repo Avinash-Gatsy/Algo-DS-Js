64bbd967e2325ae476f09ddc34e191a4c9a19964
Avinash <avinashg12291992@gmail.com>
2021-04-15 22:50:39 +0530

    insertion sort

---
+++ snippets/insertionSort.js
@@ -0,0 +1,12 @@
+function insertionSort(arr){
+    for(var i=1; i < arr.length; i++){
+        let curr = arr[i];
+        for(var j = i-1; j>=0 && arr[j] > curr; j--){
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = curr;
+    }
+    return arr;
+}
+insertionSort([8,3,15,22,9,1]);
+// [1, 3, 8, 9, 15, 22]
